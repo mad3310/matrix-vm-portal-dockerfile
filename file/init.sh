379f07c1815f786bc455cfad6cb200a2cc523720
liuhao1 <liuhao1@letv.com>
2015-11-26 10:11:26 +0800

    modify jetty port to 8080;modify hosts config

--- file/init.sh
+++ file/init.sh
@@ -5,3 +5,7 @@ cd lcp
 jar -xvf /opt/letv/jetty/webapps/lcp.war
 cp /opt/letv/jetty/jetty-web.xml /opt/letv/jetty/webapps/lcp/WEB-INF/
 rm -rf /opt/letv/jetty/webapps/lcp.war
+
+umount /etc/hosts
+echo -e "10.176.127.32 matrix.letvcloud.com" >> hosts
+echo -e "10.150.149.34 uc.letvcloud.com" >> hosts
